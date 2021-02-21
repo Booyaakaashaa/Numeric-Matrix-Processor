7e4fef1204909d263a8633b945c06c61be9fc6e2
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-22 03:17:16 +0530

    Mat Scalar mul - 2/6

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -4,16 +4,10 @@ arr1 = []
 for i in range(m):
     arr1.append(input().strip().split())
     arr1[i] = [int(k) for k in arr1[i]]
+scalar = int(input())
 
-arr2 = []
-for i in range(x):
-    arr2.append(input().strip().split())
-    arr2[i] = [int(k) for k in arr2[i]]
 add = [[0] * n] * m
-if m == x and n == y:
-    for i in range(m):
-        for j in range(n):
-            add[i][j] = arr1[i][j] + arr2[i][j]
-        print(*add[i])
-else:
-    print("ERROR")
+for i in range(m):
+    for j in range(n):
+        add[i][j] = arr1[i][j] * scalar
+    print(*add[i])

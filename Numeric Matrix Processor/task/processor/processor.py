ecdd9d2300d6ae2005e508e4c4ddaf3765744e04
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-22 03:09:55 +0530

    Mat add

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,1 +1,21 @@
-print('Hello, world!')
+import sys
+m, n = input().strip().split()
+m, n = int(m), int(n)
+arr1 = []
+for i in range(m):
+    arr1.append(input().strip().split())
+    arr1[i] = [int(k) for k in arr1[i]]
+x, y = input().strip().split()
+x, y = int(x), int(y)
+arr2 = []
+for i in range(x):
+    arr2.append(input().strip().split())
+    arr2[i] = [int(k) for k in arr2[i]]
+add = []
+if m == x and n == y:
+    for i in range(m):
+        for j in range(n):
+            add[i][j] = arr1[i][j] + arr2[i][j]
+        print(*add[i])
+else:
+    print("ERROR", file=sys.stderr)

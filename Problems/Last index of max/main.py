c5097c72dc6dce3081794d5e78d02ee87f9458b5
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-24 03:20:48 +0530

    Mat Calculator - transpose added -4/6

---
+++ Problems/Last index of max/main.py
@@ -0,0 +1,10 @@
+def last_indexof_max(numbers):
+    ind = -1
+    top = -100000
+    for i in range(len(numbers)):
+        if numbers[i] >= top:
+            top = numbers[i]
+            ind = i
+
+
+    # write the modified algorithm here
